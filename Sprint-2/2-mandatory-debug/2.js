c929c3c64eb01ca3bb073a24f75e2a7cbc67ad6c
Ammad <anosidium@hotmail.com>
2025-10-21 14:05:20 +0100

    Completed 2.js exercise

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -3,10 +3,12 @@
 // Predict the output of the following code:
 // =============> Write your prediction here
 
+// It will convert 103 number into a String representation and then will either do no manipulations or invert the string
+
 const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+function getLastDigit(digit) {
+  return digit.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
@@ -22,3 +24,7 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+// The function getLastDigit() has no parameters and referenced a globally scoped constant `num`.
+// In order for it to `work properly`, a parameter is required and referencing it in order to
+// return an expected value
