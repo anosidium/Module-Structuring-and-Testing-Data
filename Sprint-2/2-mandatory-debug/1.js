4bf33763cae2da0d60b6c8cf1e8e63ff96dd5ccc
Ammad <anosidium@hotmail.com>
2025-10-21 14:00:23 +0100

    Completed 1.js exercise

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// It will not say `undefined` but will not add parameter `a` and `b`
+
 function sum(a, b) {
   return;
   a + b;
@@ -9,5 +11,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+
+// The console prints: The sum of 10 and 32 is undefined
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function add(a, b) {
+  return a + b;
+}
