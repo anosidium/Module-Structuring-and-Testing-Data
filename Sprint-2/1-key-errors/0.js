e1af344e710f662ce0b211390ec697d86ac981cb
Ammad <anosidium@hotmail.com>
2025-10-21 13:32:38 +0100

    Completed 0.js exercise

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,10 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// I predict that the function will capitalise the first character of the string
+// And then return a string using string interpolation using the first character of the string
+// and a slice of the remaining characters of the string excluding the first character.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +14,13 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+
+// An error is thrown because of the variable name collision. The function has a parameter called `str`
+// but in the body of the function, a variable `str` is declared and that conflicts with the parameter.
+
 // =============> write your new code here
+
+function upperCased(string) {
+  const capitalised = `${string[0].toUpperCase()}${string.slice(1)}`;
+  return capitalised;
+}
