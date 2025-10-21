1028541bd4f7dc41e446c4b5ed85a09e55981e6e
Ammad <anosidium@hotmail.com>
2025-10-21 13:39:02 +0100

    Completed 1.js exercise

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,6 +3,9 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+// Because of the variable name collision. The function parameter is called decimalNumber
+// but another decimalNumber is declared inside the body of the function.
+
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -16,5 +19,14 @@ console.log(decimalNumber);
 
 // =============> write your explanation here
 
+// The console prints: SyntaxError: Identifier 'decimalNumber' has already been declared
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function percentaged(decimalNumber) {
+  const constant = 0.5;
+  const percentage = `${constant * 100}%`;
+
+  return percentage;
+}
