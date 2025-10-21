f342a4cbb909efbfa0d7889e1d7adfd121c82abf
Ammad <anosidium@hotmail.com>
2025-10-21 13:43:57 +0100

    Completed 2.js exercise

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -5,16 +5,26 @@
 
 // =============> write your prediction of the error here
 
+// The function will not execute because of the undefined variable `num` and the parameter `3`,
+// which is not allowed to be defined with just numbers. There must be letters to define a variable
+// but can begins with numbers.
+
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
 
+// SyntaxError: Unexpected number
+
 // =============> explain this error message here
+
+// It's referring to the function parameter `3`
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function squared(value) {
+    return value * value;
+}
