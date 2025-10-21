01dccfc4206ec7505ee68860eb6e7deecaf9e5a5
Ammad <anosidium@hotmail.com>
2025-10-21 13:53:29 +0100

    Completed 0.js exercise

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -2,6 +2,8 @@
 
 // =============> write your prediction here
 
+// The function prints to the console by multiplying the parameter `a` and `b`
+
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -10,5 +12,14 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
 
+// I missed line 11, the function does indeed prints to the console but returns nothing
+// which explains `undefined` in the string interpolation of the function
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function product(a, b) {
+  const result = a * b;
+  console.log(result)
+  return result;
+}
