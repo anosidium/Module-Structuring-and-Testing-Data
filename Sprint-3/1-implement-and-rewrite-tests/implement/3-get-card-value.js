c947491bfdf54448d91b37be0cde2a2178d518a7
Ammad <anosidium@hotmail.com>
2025-10-22 21:41:43 +0100

    Completed 3-get-card-value.js exercise

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -8,8 +8,16 @@
 // write one test at a time, and make it pass, build your solution up methodically
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
+  const rank = card[0];
+
   if (rank === "A") {
     return 11;
+  } else if (["J", "Q", "K"].includes(rank)) {
+    return 10;
+  } else if (rank >= 2 || rank <= 10) {
+    return Number(rank);
+  } else {
+    return "Invalid card rank.";
   }
 }
 
@@ -39,19 +47,25 @@ assertEquals(aceofSpades, 11);
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
 const fiveofHearts = getCardValue("5♥");
-// ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(fiveofHearts, 5);
 
 // Handle Face Cards (J, Q, K):
 // Given a card with a rank of "10," "J," "Q," or "K",
 // When the function is called with such a card,
 // Then it should return the value 10, as these cards are worth 10 points each in blackjack.
+const jackOfDiamonds = getCardValue("J♢");
+assertEquals(jackOfDiamonds, 10);
 
 // Handle Ace (A):
 // Given a card with a rank of "A",
 // When the function is called with an Ace,
 // Then it should, by default, assume the Ace is worth 11 points, which is a common rule in blackjack.
+const ace = getCardValue("A");
+assertEquals(ace, 11);
 
 // Handle Invalid Cards:
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+const invalidRank = getCardValue("@");
+assertEquals(invalidRank, "Invalid card rank.");
