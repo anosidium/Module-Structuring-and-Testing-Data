0631bd4df952eee09bea83d183a56c7c06a47cb9
Ammad <anosidium@hotmail.com>
2025-10-22 21:58:12 +0100

    Completed 2-is-proper-fraction.test.js exercise

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an import fraction", () => {
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative fraction", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for a fraction whose numerator and denominator are the same", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
