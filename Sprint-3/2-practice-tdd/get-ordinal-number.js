7536db77a606f96d68bdc503dc8a196f9368f4b8
Ammad <anosidium@hotmail.com>
2025-10-25 13:00:34 +0100

    Completed get-ordinal-number exercise

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,30 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  if (isNaN(num)) {
+    return "";
+  }
+
+  const numberString = String(num);
+  const lastDigit = Number(numberString.slice(-1));
+  const lastDigits = Number(numberString.slice(-2));
+  let ordinal;
+
+  console.log("lastDigits", lastDigits);
+
+  if (num === 0) {
+    ordinal = "";
+  } else if (lastDigits === 11) {
+    ordinal = "th";
+  } else if (lastDigit === 1) {
+    ordinal = "st";
+  } else if (lastDigit === 2) {
+    ordinal = "nd";
+  } else if (lastDigit === 3) {
+    ordinal = "rd";
+  } else {
+    ordinal = "th";
+  }
+
+  return `${num}${ordinal}`;
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -8,6 +8,54 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
-  expect(getOrdinalNumber(1)).toEqual("1st");
+test("should return '@' for ", () => {
+  expect(getOrdinalNumber("@")).toEqual("");
+});
+
+test("should return '0' for 0", () => {
+  expect(getOrdinalNumber(0)).toEqual("0");
+});
+
+test("should return '11th' for 11", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+});
+
+test("should return '-11th' for -11", () => {
+  expect(getOrdinalNumber(-11)).toEqual("-11th");
+});
+
+test("should return `22nd` for 22", () => {
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+});
+
+test("should return `123rd` for 123", () => {
+  expect(getOrdinalNumber(123)).toEqual("123rd");
+});
+
+test("should return '1234th' for 1234", () => {
+  expect(getOrdinalNumber(1234)).toEqual("1234th");
+});
+
+test("should return '12345th' for 12345", () => {
+  expect(getOrdinalNumber(12345)).toEqual("12345th");
+});
+
+test("should return '123456th' for 123456", () => {
+  expect(getOrdinalNumber(123456)).toEqual("123456th");
+});
+
+test("should return '1234567th' for 1234567", () => {
+  expect(getOrdinalNumber(1234567)).toEqual("1234567th");
+});
+
+test("should return '12345678th' for 12345678", () => {
+  expect(getOrdinalNumber(12345678)).toEqual("12345678th");
+});
+
+test("should return '123456789th' for 123456789", () => {
+  expect(getOrdinalNumber(123456789)).toEqual("123456789th");
+});
+
+test("should return '1234567890' for 1234567890th", () => {
+  expect(getOrdinalNumber(1234567890)).toEqual("1234567890th");
 });
