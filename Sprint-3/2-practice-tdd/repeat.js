fb189461629ebb596bc77996b9d52efc833655ce
Ammad <anosidium@hotmail.com>
2025-10-25 15:30:10 +0100

    Completed repeat exercise

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,15 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(string, count) {
+  if (isNaN(count)) {
+    throw new Error("count is not a number");
+  } else if (count < 0) {
+    throw new Error("negative numbers are not valid");
+  } else if (count === 0) {
+    return "";
+  } else if (count === 1) {
+    return string;
+  } else {
+    return string.repeat(count);
+  }
 }
 
 module.exports = repeat;
--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -21,12 +21,35 @@ test("should repeat the string count times", () => {
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("should repeat the string 1 times", () => {
+  const str = "Hello, World!";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual(str);
+});
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
 
+test("should return an empty string for 0 times", () => {
+  const str = "Bye, World!";
+  const count = 0;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
+
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+
+test("should throw an error for a negative number", () => {
+  expect(() => repeat("Ello!", -1)).toThrow("negative numbers are not valid");
+});
+
+// case: count is not a number:
+test("should throw an error when count is not a number", () => {
+  expect(() => repeat("Hello?", "@")).toThrow("count is not a number");
+});
