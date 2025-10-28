3aa28fa1318f6f1aa478392751c95ea4c726d042
Ammad <anosidium@hotmail.com>
2025-10-28 02:13:28 +0000

    Amend the range explanation using the notation

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -8,8 +8,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
-// num represents a random number in an inclusive range from 1 to 100
+// num represents a random number in the interval [1,100]
 // First, it starts with 100 - 1 + 1, which is equals to 100
-// Secondly, it multiply with Math.random(), which the function returns any number between 0 and 1, inclusive.
+// Secondly, it multiply with Math.random(), which the function returns any number in the interval [0,1]
 // Thirdly, Math.floor() rounds down and returns the largest integer less than or equal to a given number
 // Finally, it adds 1
